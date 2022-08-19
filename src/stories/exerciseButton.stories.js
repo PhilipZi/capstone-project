121e37b7380b157872739938d361564ce703e4ba
Philip Zimmermann <philip.zimmermann@freenet.de>
2022-08-19 13:04:17 +0200

    add to storybook

---
+++ src/stories/exerciseButton.stories.js
@@ -0,0 +1,8 @@
+import ExerciseButton from '../components/exerciseButton/exerciseButton';
+
+export default {
+	title: 'ExerciseButton',
+	component: ExerciseButton,
+};
+
+export const ButtonExercise = () => <ExerciseButton />;
