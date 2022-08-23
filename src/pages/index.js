9f3af75587080ad3cc2f517558c035cd9dc445db
Philip Zimmermann <philip.zimmermann@freenet.de>
2022-08-23 13:25:38 +0200

    change capital letter for BackArrowDown

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 
 import ExerciseCard from '../components/ExerciseCard';
-import BlackArrowDown from '../components/Icons/blackArrowDown';
+import BlackArrowDown from '../components/Icons/BlackArrowDown';
 import StyledSetupContainer from '../components/StyledSetupContainer';
 
 export default function HomePage() {
