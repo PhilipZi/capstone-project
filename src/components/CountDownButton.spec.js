b057f95021309049fa487b5f02c1e4b6f16a4df3
Philip Zimmermann <philip.zimmermann@freenet.de>
2022-09-09 11:35:52 +0200

    edit skip to test Countdown button

--- src/components/CountDownButton.spec.js
+++ src/components/CountDownButton.spec.js
@@ -1,27 +1,27 @@
 /**
  * @jest-environment jsdom
  */
-// import {render, renderHook, screen} from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import '@testing-library/jest-dom';
+import {render, renderHook, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 
-// import useStore from '../hooks/useStore';
+import useStore from '../hooks/useStore';
 
-// import CountDownButton from './CountDownButton';
+import CountDownButton from './CountDownButton';
 
-// describe('CountDownButton component', () => {
-// 	it('should render children', () => {
-// 		render(<CountDownButton />);
-// 		const countDownButton = screen.getByRole('button');
-// 		expect(countDownButton).toBeInTheDocument();
-// 	});
+describe('CountDownButton component', () => {
+	it('should render children', () => {
+		render(<CountDownButton />);
+		const countDownButton = screen.getByRole('button');
+		expect(countDownButton).toBeInTheDocument();
+	});
 
-// 	it('should allow clicks', async () => {
-// 		const store = renderHook(() => useStore());
-// 		const {changeToFalse} = store.result.current;
-// 		render(<CountDownButton />);
-// 		const countDownButton = screen.getByTestId('countdownbutton');
-// 		await userEvent.click(countDownButton);
-// 		expect(changeToFalse).toBeCalled();
-// 	});
-// });
+	it.skip('should allow clicks', async () => {
+		const store = renderHook(() => useStore());
+		const {changeToFalse} = store.result.current;
+		render(<CountDownButton />);
+		const countDownButton = screen.getByTestId('countdownbutton');
+		await userEvent.click(countDownButton);
+		expect(changeToFalse).toBeCalled();
+	});
+});
