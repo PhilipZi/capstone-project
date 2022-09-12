c734bbf518bfba4500a0df1deadb1b8fd3bac504
Philip Zimmermann <philip.zimmermann@freenet.de>
2022-09-12 15:39:13 +0200

    edit StyledNameApp

--- src/components/StyledNameApp.js
+++ src/components/StyledNameApp.js
@@ -1,14 +1,18 @@
 import styled from 'styled-components';
 
 const StyledNameApp = styled.h1`
-	position: absolute;
-	top: 8%;
-	left: 50%;
-	width: 100%;
-	transform: translate(-50%, 50%);
-	color: white;
 	font-family: 'Bungee Shade', sans-serif;
 	font-size: 3rem;
+	color: white;
+
 	text-align: center;
+	&::before {
+		content: '';
+		position: absolute;
+		top: 8%;
+		left: 50%;
+		width: 100%;
+		transform: translate(-50%, 50%);
+	}
 `;
 export default StyledNameApp;
